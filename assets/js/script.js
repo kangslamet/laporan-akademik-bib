9dfada15652af858658e3347149f5ad1c0aa899a
Slamet Mulyani <slametcute@gmail.com>
2025-06-25 23:00:10 +0700

    Updated scrit.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,23 @@ function cariData() {
   const input = document.getElementById("searchInput").value.trim();
   const hasilDiv = document.getElementById("hasil");
 
-  const hasil = data.find((item) => item.nim === input);
+  const hasil = data.find((item) => item["NIM"] === input);
 
   if (hasil) {
     hasilDiv.innerHTML = `
       <h3 style="color: #28a745;">Data Ditemukan</h3>
-      <p><strong>Nama:</strong> ${hasil.nama}</p>
-      <p><strong>Prodi:</strong> ${hasil.prodi}</p>
-      <p><strong>IPK:</strong> ${hasil.ipk}</p>
-      <p><strong>Semester:</strong> ${hasil.semester}</p>
+      <p><strong>Nama Lengkap:</strong> ${hasil["Nama Lengkap"]}</p>
+      <p><strong>Tempat & Tanggal Lahir:</strong> ${hasil["Tempat & Tanggal Lahir"]}</p>
+      <p><strong>Jenis Kelamin:</strong> ${hasil["Jenis Kelamin"]}</p>
+      <p><strong>Nomor WA:</strong> ${hasil["Nomor WA"]}</p>
+      <p><strong>Semester:</strong> ${hasil["Semester"]}</p>
+      <p><strong>IPK:</strong> ${hasil["IPK"]}</p>
+      <p><strong>KRS:</strong> ${hasil["KRS"]}</p>
+      <p><strong>KHS:</strong> ${hasil["KHS"]}</p>
+      <p><strong>Kegiatan Akademik:</strong> ${hasil["Kegiatan Akademik"]}</p>
+      <p><strong>Kegiatan Non Akademik:</strong> ${hasil["Kegiatan Non Akademik"]}</p>
+      <p><strong>Sertifikat/Dokumen lainnya:</strong> ${hasil["Sertifikat/ Dokumen lainnya"]}</p>
+      <p><strong>Foto Kegiatan:</strong> <a href="${hasil["Foto Kegiatan"]}" target="_blank">Lihat Foto</a></p>
     `;
   } else {
     hasilDiv.innerHTML = `
